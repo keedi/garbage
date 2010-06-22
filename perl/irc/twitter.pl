9e8b6010778f8eddbfcd3e86acf71dac4c28eaab
Keedi Kim <keedi.k@gmail.com>
2010-06-22 15:57:29 +0900

    Add twitter bot

---
+++ perl/irc/twitter.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/env perl
+
+use utf8;
+
+package TwitBot;
+
+use parent 'Bot::BasicBot';
+use Encode qw/decode encode/;
+use LWP::Simple;
+
+sub said {
+    my ( $self, $args ) = @_;
+
+    $args->{body} =~ m|http://twitter.com/(.*?)/status/\d+|;
+    my $id = $1;
+    my $url = $&;
+
+    my $content = get( $url );
+    if ($content =~ m|<meta content="(.*?)" name="description" />|) {
+        my $msg = $1;
+        $self->say(
+            channel => $args->{channel},
+            body    => "\@$id: ".encode('utf-8', $msg),
+        );
+    }
+}
+
+1;
+
+package main;
+
+use common::sense;
+use Bot::BasicBot;
+
+my $bot = TwitBot->new(
+    server   => 'irc.freenode.org',
+    nick     => 'twitbot',
+    channels => [ '#perl-kr', ],
+    encoding => 'utf-8',
+)->run;
